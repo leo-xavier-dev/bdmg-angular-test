14b318172891df6da5fb49a76eb342c63723c608
leo-xavier-dev <prof.leonardoxavier@gmail.com>
2023-08-31 17:52:40 -0300

    chore: Primeiro commit para publicação v5

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
 const express = require('express');
-
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
-
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
@@ -19,7 +6,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
