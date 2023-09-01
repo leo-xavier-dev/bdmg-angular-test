1dae08baed5b16318a6ce6a9ac91f6fcead9b9f0
leo-xavier-dev <prof.leonardoxavier@gmail.com>
2023-08-31 21:16:20 -0300

    chore: Primeiro commit para publicação v10

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) => {
+app.get('', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
