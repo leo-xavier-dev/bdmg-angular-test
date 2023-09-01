9f15727307899b9b99fedb830dadbcacbd53c93b
leo-xavier-dev <prof.leonardoxavier@gmail.com>
2023-08-31 21:50:44 -0300

    chore: Primeiro commit para publicação v11

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
-
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-app.get('', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});
-
-app.listen(process.env.PORT || 4200);
+app.use(express.static(__dirname + '/dist/bdmg-angular-test'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/bdmg-angular-test/index.html'));});
+app.listen(process.env.PORT || 8080);
